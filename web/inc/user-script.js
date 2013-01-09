f6f07e44d6356701eb14017ed98b6ed076b108a6
Meriem <Meriem@satellite>
2013-01-09 22:41:45 +0100

    Le lien Delete (liste des utilisateurs) refonctionne

--- web/inc/user-script.js
+++ web/inc/user-script.js
@@ -1,6 +1,7 @@
 $(function() {
     var A_USER_UPDATE = 'a[id^="userupdate"]';
     var A_USER_ADD = 'a[id="useradd"]';
+    var A_USER_DELETE = 'a[id="userdelete"]';
     var DIV_USERS_LIST = 'div[id="users"]';
     var DIV_USER_UPDATE = 'div[id^="userupdate"]';
     var DIV_USER_ADD = 'div[id="useradd"]';
@@ -9,13 +10,7 @@ $(function() {
         collapsible:true,
         active:false,
         header: 'h3'
-   });
-
-   $("#userdelete").click(function() {
-      window.location = $(this).attr('href');
-      return false;
-   });
-   
+    });
    
     $(DIV_USER_ADD +','+DIV_USER_UPDATE ).dialog({
         autoOpen: false,
@@ -34,6 +29,10 @@ $(function() {
             document.getElementById('updateform').action=action+'?id='+currentUserId;
         }
         $(DIV_USER_UPDATE).dialog("open");
+    });
+    
+    $(A_USER_DELETE).click(function() {
+        document.location.href=$(this).attr('href');
     });
    
 });
